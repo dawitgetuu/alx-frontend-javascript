f9499b686198dcca3f263a76dd86853fb217e1b9
dawit getuu <archfun3@gmail.com>
2024-07-02 12:23:15 +0000

    add task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,10 @@
-const groceriesList = () => {
-  const map = new Map();
-  map.set('Apples', 10);
-  map.set('Tomatoes', 10);
-  map.set('Pasta', 1);
-  map.set('Rice', 1);
-  map.set('Banana', 5);
-  return map;
-};
+export default function groceriesList() {
+  const items = new Map();
+  items.set('Apples', 10);
+  items.set('Tomatoes', 10);
+  items.set('Pasta', 1);
+  items.set('Rice', 1);
+  items.set('Banana', 5);
 
-export default groceriesList;
+  return items;
+}
